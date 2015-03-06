1503ddd36fe4c630863166a2430d8cdd2f2eddf1
Ben Meier <benmeier42@gmail.com>
2015-03-06 21:06:01 +0200

    Added tests for operations

---
+++ tests/test_operations.py
@@ -0,0 +1,30 @@
+import pytest
+
+from validoot.operations import And, Or
+from validoot.exceptions import ValidationError
+
+def tt(v):
+    return True
+
+def ff(v):
+    return False
+
+def test_And():
+    assert And(tt, tt)(1)
+
+    with pytest.raises(ValidationError):
+        And(tt, ff)(1)
+
+def test_And_bad_arg():
+    with pytest.raises(TypeError):
+        And(object(), tt)
+
+def test_Or():
+    assert Or(ff, tt)(1)
+
+    with pytest.raises(ValidationError):
+        Or(ff, ff)(1)
+
+def test_Or_bad_arg():
+    with pytest.raises(TypeError):
+        Or(object(), tt)
